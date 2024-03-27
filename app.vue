299af46e34593d2b3c351e593b5fe2f7cb676961
Alan Maldonado <alan.m@incfile.com>
2024-03-27 14:58:23 -0600

    First form

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
+<script setup>
+const formData = ref({
+  username: 'alanmaldonado',
+  password: '',
+});
+
+async function handleSubmit(data) {
+  await wait(3000);
+  console.log(data);
+}
+</script>
 <template>
   <div>
-    <NuxtWelcome />
+    <FormKit type="form" :value="formData" @submit="handleSubmit">
+      <h1>Login</h1>
+      <FormKit type="text" label="Username" name="username" />
+      <FormKit type="password" label="Password" name="password" />
+    </FormKit>
   </div>
 </template>
